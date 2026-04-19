9d060332d04002873de7e528585663a46c81a618
Rafael Coll <rafaelcoll@msn.com>
2026-04-19 02:20:32 -0300

    Add initial project structure

---
+++ Anticompressor/src/Anticompressor.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Anticompressor {
+    Map<String, String> mapaDicionario = new HashMap<>();
+
+    public Anticompressor(String[][] dicionario) {
+        for (String[] par : dicionario) {
+            mapaDicionario.put(par[0], par[1]);
+        }
+    }
+
+    public String anticompressor(String[][] dicionario) {
+        StringBuilder resultadoFinal = new StringBuilder();
+        String valorInicial = mapaDicionario.get(dicionario[0][0]);
+        System.out.println("Valor inicial: " + valorInicial);
+
+        resultadoFinal.append(adicionaValor(valorInicial, mapaDicionario));
+        
+        return resultadoFinal.toString();
+    }
+    
+    private static String buscaValor(String chave, Map<String, String> mapaDicionario) {
+        String valor = mapaDicionario.get(chave);
+        if (valor == null || valor.isEmpty()) {
+            return chave;
+        }
+        return adicionaValor(valor, mapaDicionario);
+    }
+    
+    private static String adicionaValor(String valor, Map<String, String> mapaDicionario) {
+        StringBuilder resultado = new StringBuilder();
+        for (char c : valor.toCharArray()) {
+            resultado.append(buscaValor(String.valueOf(c), mapaDicionario));
+        }
+        return resultado.toString();
+    }
+
+    public String stringDicionario(String[][] dicionario) {
+        StringBuilder resultado = new StringBuilder();
+        for (String[] linha : dicionario) {
+            // Ignorar entradas inválidas
+            if (linha == null || linha.length != 2) {
+                continue; 
+            }
+
+            String chave = linha[0];
+            String valor = linha[1];
+            resultado.append(chave).append(" ").append(valor).append("\n");
+        }
+        return resultado.toString();
+    }
+
+}
---
+++ Anticompressor/src/App.java
@@ -0,0 +1,54 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Uso: java App <caminho_dicionario>");
+            return;
+        }
+
+        System.out.println("Carregando dicionário... " + args[0]);
+        String[][] dicionario = carregarDicionario(args[0]);
+        Anticompressor anticompressor = new Anticompressor(dicionario);
+
+        System.out.println("Dicionário carregado com sucesso.");
+        System.out.println(anticompressor.stringDicionario(dicionario));
+
+        long inicio = System.currentTimeMillis();
+        String result = anticompressor.anticompressor(dicionario);
+        long fim = System.currentTimeMillis();
+        // System.out.println("Resultado: " + result);
+        System.out.println("Comprimento do resultado: " + result.length());
+        System.out.println("Tempo de execução: " + (fim - inicio) + "ms");
+    }
+
+    private static String[][] carregarDicionario(String caminhoArquivo) {
+        String[][] pares;
+
+        try {
+            List<String> linhas = Files.readAllLines(Path.of(caminhoArquivo));
+            pares = new String[linhas.size()][2];
+
+            for (int i = 0; i < linhas.size(); i++) {
+                String[] linha = linhas.get(i).split(" ", 2);
+
+                String chave;
+                String valor;
+                chave = linha[0];
+                valor = linha[1];
+
+                if (chave.isEmpty()) {
+                    throw new RuntimeException("Formato inválido na linha: " + linha);
+                }
+
+                pares[i] = new String[] { chave, valor };
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao ler dicionario: " + caminhoArquivo, e);
+        }
+
+        return pares;
+    }
+}
